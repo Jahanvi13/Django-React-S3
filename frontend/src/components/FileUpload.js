49cd796f6ced8da373c3450e129a4a833e830273
Jahanvi Agarwal <97488922+Jahanvi13@users.noreply.github.com>
2024-06-07 09:59:57 -0400

    Update FileUpload.js

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,50 +1,37 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './FileUpload.css';
 
 const UploadFile = () => {
     const [file, setFile] = useState(null);
-    const [error, setError] = useState('');
-
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
     };
 
-    const handleSubmit = async (e) => {
+    const handleUploadFile = async (e) => {
         e.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+
         try {
-            const formData = new FormData();
-            formData.append('file', file);
-
-            const response = await fetch('http://localhost:8000/api/upload/', {
-                method: 'POST',
-                body: formData
+            const response = await axios.post('/api/upload/', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
             });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            const data = await response.json();
-            console.log(data);
-            // Redirect or show success message as needed
+            console.log(response.data);
         } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-            setError('Error uploading file');
+            console.error(error);
         }
     };
 
     return (
         <div className="upload-container">
-            <form className="upload-form" onSubmit={handleSubmit}>
-                <h2>Upload File</h2>
-                {error && <p className="error">{error}</p>}
-                <div>
-                    <label>
-                        Choose File:
-                        <input type="file" onChange={handleFileChange} />
-                    </label>
-                </div>
+            <h2>Upload Files</h2>
+            <form onSubmit={handleUploadFile}>
+                <input type="file" onChange={handleFileChange} />
+                
                 <button type="submit">Upload</button>
             </form>
         </div>
